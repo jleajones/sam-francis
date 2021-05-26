0e66dfd667c3b64d59d7d2ece3ecd778b317a7dc
Jason Lea-Jones <jleajones@gmail.com>
2021-05-26 10:41:17 -0400

    updates to remove guest input

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,16 +3,14 @@ import sgMail from '@sendgrid/mail';
 export default async function(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const { name, email, rsvp, guestName, guestEmail, guestRsvp, message } = req.body;
+  const { name, email, rsvp, message } = req.body;
 
   const content = {
     to: 'samuelfrancis1119@gmail.com',
     from: 'samuelfrancis1119@gmail.com',
     subject: `Wedding RSVP from - ${email}`,
-    text: `${name} ${guestName ? `| ${guestName}` : ''}`,
-    html: `<h5>Primary Guest: ${name}  (${email})</h5> <p> RSVP Status: ${rsvp}</h5>
-      ${guestName.length > 1 ? `<p>Additional Guest: ${guestName} (${guestEmail}) | RSVP Status ${guestRsvp}</p>` : ''}
-    <p>${message}</p>`
+    text: `${name}`,
+    html: `<h5>Primary Guest: ${name}  (${email})</h5> <p> RSVP Status: ${rsvp}</h5><p>${message}</p>`
   }
 
   try {
